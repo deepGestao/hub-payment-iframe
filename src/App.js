386ba0d7e20c11ede8d2c365ab8b218d8c0a18da
k.tavares.martins <k.tavares.martins@accenture.com>
2024-09-20 17:33:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { initMercadoPago, Payment } from "@mercadopago/sdk-react";
+initMercadoPago(process.env.REACT_APP_MERCADO_PAGO_PUBLIC_KEY);
 
 function App() {
+
+  const onSubmit = async ({ selectedPaymentMethod, formData }) => {
+    console.log({ selectedPaymentMethod, formData });
+  };
+
+  const initialization = {
+    amount: 100,
+  };
+  const customization = {
+    paymentMethods: {
+      ticket: "all",
+      bankTransfer: "all",
+      creditCard: "all",
+      mercadoPago: "all",
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Payment
+        customization={customization}
+        initialization={initialization}
+        onSubmit={onSubmit}
+      />
     </div>
   );
 }
